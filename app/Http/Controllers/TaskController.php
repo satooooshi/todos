245bf4b888460f7182b4c89fd165f6e2fbeb01af
Aikawa Satoshi <aikawa@spookies.co.jp>
2021-04-29 18:58:42 +0900

    chapter4 finished.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
-        //return "Hello world";
+        // すべてのフォルダを取得する
         $folders = Folder::all();
-        \Log::debug($folders);
-
+    
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+    
+        // 選ばれたフォルダに紐づくタスクを取得する
+        //$tasks = Task::where('folder_id', $current_folder->id)->get();
+        $tasks = $current_folder->tasks()->get();
+    
         return view('tasks/index', [
-            //key == variable in template => values
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
